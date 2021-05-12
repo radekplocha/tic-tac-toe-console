59d70abc4f668fd3ea61ac6a432c584b76c00b47
rplocha <72463345+rplocha@users.noreply.github.com>
2021-05-12 11:35:33 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,83 @@
+board = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+
+player1 = 'X'
+player2 = 'O'
+game_is_in_progress = True
+turn = 1
+game_round = 1
+
+
+def draw_board(b):
+    for row in b:
+        print(f" {row[0]} | {row[1]} | {row[2]} ")
+        if board.index(row) != 2:
+            print("-----------")
+
+
+def check_for_choice(b, ch):
+    for row in b:
+        for num in row:
+            if num == ch:
+                return row.index(num), board.index(row)
+
+
+def check_for_win(b):
+    if board[0][0] == board[0][1] == board[0][2]:
+        return True
+    if board[1][0] == board[1][1] == board[1][2]:
+        return True
+    if board[2][0] == board[2][1] == board[2][2]:
+        return True
+    if board[0][0] == board[1][0] == board[2][0]:
+        return True
+    if board[0][1] == board[1][1] == board[2][1]:
+        return True
+    if board[0][2] == board[1][2] == board[2][2]:
+        return True
+    if board[0][0] == board[1][1] == board[2][2]:
+        return True
+    if board[0][2] == board[1][1] == board[2][0]:
+        return True
+
+
+while game_is_in_progress:
+    print('\n' * 5)
+    draw_board(board)
+    if turn == 1:
+        choice = int(input(f"PLAYER{turn} choose a place: "))
+        try:
+            x, y = check_for_choice(board, choice)
+        except TypeError:
+            continue
+        board[y][x] = player1
+    else:
+        choice = int(input(f"PLAYER{turn} choose a place: "))
+        try:
+            x, y = check_for_choice(board, choice)
+        except TypeError:
+            continue
+        board[y][x] = player2
+
+    if game_round == 9 and not check_for_win(board):
+        print('\n' * 5)
+        draw_board(board)
+        print("DRAW")
+        game_is_in_progress = False
+        break
+    if check_for_win(board):
+        print('\n' * 5)
+        draw_board(board)
+        print(f'PLAYER{turn} WON')
+        game_is_in_progress = False
+        break
+    if turn == 1:
+        turn = 2
+    else:
+        turn = 1
+    game_round += 1
+
+
